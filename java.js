3b40169bd8b7611f6c3924a13caae565f6d1646b
Fozil5598 <fozil5598qurbonov@gmail.com>
2022-09-08 18:55:17 +0500

    1

---
+++ java.js
@@ -0,0 +1,56 @@
+// const prompt = require('prompt-sync')
+// let products = [
+//     { name: 'Coca Cola 1L',     price: 8000, category: 'drink' },
+//     { name: 'Snikers',          price: 5000, category: 'sweet' },
+//     { name: 'Coca Cola 0.5L',   price: 5000, category: 'drink' },
+//     { name: 'Fanta 1.5L',       price: 12000, category: 'drink' },
+//     { name: 'Chips',            price: 1000, category: 'sweet' },
+//     { name: 'Humson',           price: 3000, category: 'drink' },
+//     { name: 'Bounty',           price: 7000, category: 'sweet' },
+// ]
+
+// consol.log(products)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
